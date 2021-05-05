6f665793f50582daeca7a0d6f250e1c4854217df
vtttuong <vtttuong@gmail.com>
2021-05-05 22:04:32 +0700

    update dockerfile and frontend

--- blog_backend/blog/urls.py
+++
@@ -1,9 +0,0 @@
-# This is blog app's urls.py file
-
-from . import views
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
-router.register("posts", views.PostViewSet, "posts")
-
-urlpatterns = router.urls
--- blog_frontend/src/App.js
+++
@@ -1,5 +0,0 @@
-// Code above code stays is the same
-import App from './Containers/App';
-// Code below stays the same
-
-export default App;
